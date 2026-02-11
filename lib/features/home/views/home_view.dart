07f0d910f370706aa9549acaa354888d03e000d9
CodeWithRicchee <CodeWithRicchee@users.noreply.github.com>
2026-02-11 20:03:18 +0530

    done

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -13,29 +13,43 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Obx(() => IndexedStack(
-        index: controller.currentNavIndex.value,
-        children: const [
-          HomeDashboard(),
-          SearchView(),
-          MyBookingsView(),
-          ProfileView(),
+    return Obx(() {
+      final index = controller.currentNavIndex.value;
+      Widget body;
+      switch (index) {
+        case 1:
+          body = const SearchView();
+          break;
+        case 2:
+          body = const MyBookingsView();
+          break;
+        case 3:
+          body = const ProfileView();
+          break;
+        default:
+          body = const HomeDashboard();
+      }
+      return Column(
+        children: [
+          Expanded(child: body),
+          Material(
+            color: Theme.of(context).scaffoldBackgroundColor,
+            child: BottomNavigationBar(
+              currentIndex: index,
+              onTap: controller.changeNavIndex,
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: AppColors.roseGold,
+              unselectedItemColor: AppColors.coolGrey,
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Iconsax.home), activeIcon: Icon(Iconsax.home_15), label: 'Home'),
+                BottomNavigationBarItem(icon: Icon(Iconsax.search_normal), activeIcon: Icon(Iconsax.search_normal_1), label: 'Search'),
+                BottomNavigationBarItem(icon: Icon(Iconsax.calendar), activeIcon: Icon(Iconsax.calendar_1), label: 'Bookings'),
+                BottomNavigationBarItem(icon: Icon(Iconsax.user), activeIcon: Icon(Iconsax.user), label: 'Profile'),
+              ],
+            ),
+          ),
         ],
-      )),
-      bottomNavigationBar: Obx(() => BottomNavigationBar(
-        currentIndex: controller.currentNavIndex.value,
-        onTap: controller.changeNavIndex,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: AppColors.roseGold,
-        unselectedItemColor: AppColors.coolGrey,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Iconsax.home), activeIcon: Icon(Iconsax.home_15), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Iconsax.search_normal), activeIcon: Icon(Iconsax.search_normal_1), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Iconsax.calendar), activeIcon: Icon(Iconsax.calendar_1), label: 'Bookings'),
-          BottomNavigationBarItem(icon: Icon(Iconsax.user), activeIcon: Icon(Iconsax.user), label: 'Profile'),
-        ],
-      )),
-    );
+      );
+    });
   }
 }
